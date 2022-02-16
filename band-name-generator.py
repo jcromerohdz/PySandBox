4bfdfc8fb4d6730b14c3ad827c8f2a84f5df1607
jcromerohdz <jcromerohdz@gmail.com>
2022-02-15 16:56:30 -0800

    band generator function

---
+++ band-name-generator.py
@@ -0,0 +1,10 @@
+def band_name_generator():
+    city_name = input("What's name of city you grew up in? ")
+    print(city_name)
+    pets_name = input("What's your pet's name ")
+    print(pets_name)
+
+    print(f"Your band name could be {city_name} {pets_name}")
+
+
+band_name_generator()
