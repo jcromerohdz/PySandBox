7fc4142f6f5d708ee59471122d48a35a44b7e247
jcromerohdz <jcromerohdz@gmail.com>
2022-02-16 08:05:29 -0800

    Fixing some cli interface alingments

--- band-name-generator.py
+++ band-name-generator.py
@@ -1,7 +1,8 @@
 def band_name_generator():
-    city_name = input("What's name of city you grew up in? ")
+    print("Welcome to the band name generator.")
+    city_name = input("What's name of city you grew up in?\n")
     print(city_name)
-    pets_name = input("What's your pet's name ")
+    pets_name = input("What's your pet's name\n")
     print(pets_name)
 
     print(f"Your band name could be {city_name} {pets_name}")
